bdd6f9639b3673658f81b946dae7c9706b6f8c7a
Unknown <adrien.robert@skoove.com>
2018-12-05 18:17:09 +0100

    Add scrolling text

--- src/components/RadioBox.js
+++ src/components/RadioBox.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import TextTicker from 'react-native-text-ticker';
 
+import Marquee from 'react-text-marquee';
 
 import "./../styles/components/RadioBox.css";
 
@@ -16,16 +16,25 @@ export default class RadioBox extends Component {
                     Direct
                 </h3>
                 <article className={'RadioBox__emission'}>
-                    <header className={'Emission__title scroll-left'}>
-                        <TextTicker>
-                            Titre de l'émission défilant
-                        </TextTicker>
+                    <header className={'Emission__title'}>
+                        <Marquee
+                            hoverToStop={true}
+                            loop={false}
+                            leading={5}
+                            trailing={5}
+                            text="Titre de l'émission défilant"
+                        />
+
                     </header>
                     <body className={'Emission__sub-container'}>
-                        <div className={'Emission__subtitle scroll-left'}>
-                            <TextTicker>
-                                sous-titre de l'émission défilant
-                            </TextTicker>
+                        <div className={'Emission__subtitle'}>
+                            <Marquee
+                                hoverToStop={true}
+                                loop={false}
+                                leading={5}
+                                trailing={5}
+                                text="sous-titre de l\'émission défilant"
+                            />
                         </div>
                         <footer className={'Emission__date'}>
                             - date
