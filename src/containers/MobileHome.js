29f23c441640b37981f9324ec6a12d82d9337b8b
Adrien Robert <adrien.robert@skoove.com>
2019-05-08 18:08:16 +0200

    Add mobile home and accordion

---
+++ src/containers/MobileHome.js
@@ -0,0 +1,184 @@
+import React, {Component} from "react";
+import base from "../db/config";
+import _ from 'lodash';
+
+import "../styles/MobileHome.css"
+import EmissionList from "../components/Emission/EmissionList";
+import {FaAngleDown, FaAngleUp} from "react-icons/fa/index";
+import EmissionCalendar from "../components/Emission/EmissionCalendar";
+import AccordionWithHeader from "react-accordion-with-header/es/AccordionWithHeader";
+import AccordionNode from "react-accordion-with-header/es/AccordionNode";
+import AccordionHeader from "react-accordion-with-header/es/AccordionHeader";
+import AccordionPanel from "react-accordion-with-header/es/AccordionPanel";
+import InformationPanel from "./InformationPanel";
+import Archives from "./Archives";
+
+export default class MobileHome extends Component {
+  constructor(props) {
+        super(props);
+
+        this.state = {
+            categories: {},
+            emissions: {},
+            homePage: {},
+            curQuestion: {},
+            currentPageState: {},
+        };
+
+        this.isLoading = this.isLoading.bind(this);
+        this.isActivePage = this.isActivePage.bind(this);
+        this.actionCallback = this.actionCallback.bind(this);
+    }
+
+    componentDidMount() {
+        base.fetch('emissions', {
+            context: this,
+            then(data) {
+                this.setState({emissions: data});
+            }
+        });
+        base.fetch('categories', {
+            context: this,
+            then(data) {
+                this.setState({categories: data});
+            }
+        });
+        base.fetch('questions', {
+            context: this,
+            then(data) {
+                let curQuestion = _.find(data, (v) => {
+                    return v.active;
+                }) || {};
+                this.setState({curQuestion: curQuestion});
+            }
+        });
+        base.fetch('statics', {
+            context: this,
+            then(data) {
+                let homePage = _.find(data, (v) => {
+                    return v.slug === 'home';
+                });
+                this.setState({
+                    homePage: homePage,
+                });
+            }
+        });
+    }
+
+    isActivePage(key) {
+        let panel = _.find(this.state.currentPageState, (item) => {
+            return item.panel === key;
+        });
+        return panel ? panel.open : false;
+    }
+
+    actionCallback(panels) {
+        console.log('panels', panels);
+        this.setState({currentPageState: panels})
+    }
+
+    isLoading() {
+        return _.isEmpty(this.state.emissions) ||
+            _.isEmpty(this.state.categories) ||
+            _.isEmpty(this.state.homePage);
+    }
+
+    render() {
+        if (this.isLoading()) {
+            return <div className={'Loading'}>
+                <img
+                    className={'Loading__logo'}
+                    src={'images/logo_black.svg'}
+                    alt={'Olaradio logo'}
+                />
+            </div>
+        }
+        let latestEmissions = _.filter(this.state.emissions, (o) => {return new Date(o.datetime) < new Date()});
+        latestEmissions = (_.sortBy(latestEmissions, (o) => { return (o['datetime'] || 0)})).reverse().slice(0, 10);
+        let homePage = this.state.homePage;
+
+        return (
+            <div className={'MobileHome'}>
+                <AccordionWithHeader className={"MobileHome__navigator"} actionCallback={this.actionCallback}>
+                    <AccordionNode className={'MobileNavigator__item'} key={0}>
+                        <AccordionHeader
+                            className={'MobileNavigator__title'}
+                            horizontalAlignment="left"
+                            verticalAlignment="center"
+                        >
+                            <h3>Agenda</h3>
+                            { this.isActivePage(0) ? <FaAngleUp /> : <FaAngleDown /> }
+                        </AccordionHeader>
+                        <AccordionPanel className={'MobileNavigator__content'}>
+                            <EmissionCalendar
+                                emissions={this.state.emissions}
+                                onEmissionClick={this.props.onEmissionClick}
+                            />
+                        </AccordionPanel>
+                    </AccordionNode>
+                    <AccordionNode className={'MobileNavigator__item'} key={1}>
+                        <AccordionHeader
+                            className={'MobileNavigator__title'}
+                            horizontalAlignment="left"
+                            verticalAlignment="center"
+                        >
+                            <h3>News</h3>
+                            { this.isActivePage(1) ? <FaAngleUp /> : <FaAngleDown /> }
+                        </AccordionHeader>
+                        <AccordionPanel className={'MobileNavigator__content'}>
+                            <aside className={'NewsBox'}>
+                                <header className={'NewsBox__header'}>
+                                    {homePage.title}
+                                </header>
+                                <article className={'NewsBox__text'}>
+                                    {homePage.texts[0]}
+                                </article>
+                            </aside>
+                        </AccordionPanel>
+                    </AccordionNode>
+                    <AccordionNode className={'MobileNavigator__item'} key={2}>
+                        <AccordionHeader
+                            className={'MobileNavigator__title'}
+                            horizontalAlignment="left"
+                            verticalAlignment="center"
+                        >
+                            <h3>Informations</h3>
+                            { this.isActivePage(2) ? <FaAngleUp /> : <FaAngleDown /> }
+                        </AccordionHeader>
+                        <AccordionPanel className={'MobileNavigator__content'}>
+                            <InformationPanel />
+                        </AccordionPanel>
+                    </AccordionNode>
+                    <AccordionNode className={'MobileNavigator__item'} key={3}>
+                        <AccordionHeader
+                            className={'MobileNavigator__title'}
+                            horizontalAlignment="left"
+                            verticalAlignment="center"
+                        >
+                            <h3>Podcasts</h3>
+                            { this.isActivePage(3) ? <FaAngleUp /> : <FaAngleDown /> }
+                        </AccordionHeader>
+                        <AccordionPanel className={'MobileNavigator__content'}>
+                            <Archives
+                                onEmissionClick={this.props.onEmissionClick}
+                                categories={this.state.categories}
+                                emissions={this.state.emissions}
+                            />
+                        </AccordionPanel>
+                    </AccordionNode>
+                </AccordionWithHeader>
+
+                <div className={'MobileHome__bottom'}>
+                    <h2 className={'MobileHome__secondTitle'}>
+                        Derniers enregistrements
+                    </h2>
+                    <EmissionList
+                        className={'MobileHome__EmissionList'}
+                        emissions={latestEmissions}
+                        onEmissionClick={this.props.onEmissionClick}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
