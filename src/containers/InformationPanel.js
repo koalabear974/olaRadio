4723e23a660e4a78c3cb0da4d664fc5278f81c65
Adrien Robert <adrien.robert@skoove.com>
2019-05-09 22:41:33 +0200

    update information panel

--- src/containers/InformationPanel.js
+++ src/containers/InformationPanel.js
@@ -3,6 +3,13 @@ import _ from 'lodash';
 import base from "../db/config";
 
 import "../styles/containers/InformationPanel.css"
+import Information from "./Information";
+
+const PANEL_WIDTH_ZERO = 8;
+const PANEL_WIDTH_OPEN = 540;
+function simple_easing(how_much_time_has_passed) {
+    return (1 - Math.cos(how_much_time_has_passed * Math.PI)) / 2;
+}
 
 export default class InformationPanel extends Component {
     constructor(props) {
@@ -11,8 +18,46 @@ export default class InformationPanel extends Component {
             staticPages: {},
             curPage: {},
             curImages: {},
+            isOpen: false,
             loading: true,
+            panelOpen: 0,
         };
+        this.onOpenClick= this.onOpenClick.bind(this);
+        this.animatePanel = this.animatePanel.bind(this);
+        this.togglePanel = this.togglePanel.bind(this);
+    }
+
+    onOpenClick(e) {
+        e.preventDefault();
+        this.togglePanel();
+    }
+
+    togglePanel() {
+        this.animatePanel(!this.state.isOpen, Date.now());
+        this.setState({isOpen: !this.state.isOpen});
+    }
+
+    animatePanel(isOpen, start) {
+        let duration = 600;
+        let now = Date.now();
+        if (now - start >= duration) return;
+        if (this.state.panelOpenPx <= PANEL_WIDTH_ZERO && !isOpen) return;
+        let p = (now - start) / duration;
+        if (isOpen) {
+            let panelOpenPx = Math.round(PANEL_WIDTH_OPEN * simple_easing(p));
+            this.setState({panelOpenPx: panelOpenPx});
+            if (panelOpenPx >= PANEL_WIDTH_OPEN) return;
+            requestAnimationFrame(() => {
+                this.animatePanel(isOpen, start);
+            });
+        } else {
+            let panelOpenPx = Math.round(PANEL_WIDTH_OPEN - (PANEL_WIDTH_OPEN * simple_easing(p)));
+            this.setState({panelOpenPx: panelOpenPx});
+            if (panelOpenPx <= PANEL_WIDTH_ZERO) return;
+            requestAnimationFrame(() => {
+                this.animatePanel(isOpen, start);
+            });
+        }
     }
 
     componentDidMount() {
@@ -20,7 +65,7 @@ export default class InformationPanel extends Component {
             context: this,
             then(data) {
                 let curPage = _.find(data, (v) => {
-                    return v.slug === 'about';
+                    return v.slug === 'infos';
                 });
                 let curImages = _.find(data, (v) => {
                     return v.slug === 'about-images';
@@ -38,22 +83,24 @@ export default class InformationPanel extends Component {
         let curPage = this.state.curPage;
         let curImages = this.state.curImages;
 
-        if (_.isEmpty(curPage) || _.size(curPage.texts)<3) {
-            return (
-                <div className="InformationPanel">
-                    <div className={'Loading'}>
-                        <img
-                            className={'Loading__logo'}
-                            src={'images/logo_black.svg'}
-                            alt={'Olaradio logo'}
-                        />
-                    </div>
-                </div>
-            );
-        }
+
         return (
             <div className={'InformationPanel'}>
-
+                <div className="InformationPanel__title-container" onClick={this.onOpenClick}>
+                    <div className="InformationPanel__title">
+                        Informations
+                    </div>
+                </div>
+                <div
+                    className={
+                        "InformationPanel__panel "
+                    }
+                    style={{
+                        left: -(this.state.panelOpenPx + PANEL_WIDTH_ZERO) + 'px',
+                    }}
+                >
+                    <Information infoPage={curPage} infoImage={curImages} />
+                </div>
             </div>
         );
     }
