e5c41dc8d1901633dd8f6af68f650dd404361c3d
Remi Momprive <remi.momprive@gmail.com>
2019-11-16 22:10:53 +0100

    [FIX] Latest shows

--- src/containers/MobileHome.js
+++ src/containers/MobileHome.js
@@ -93,7 +93,7 @@ export default class MobileHome extends Component {
         if (this.isLoading()) {
             return (<Loading />);
         }
-        let latestEmissions = _.filter(this.state.emissions, (o) => {return new Date(o.datetime) < new Date()});
+        let latestEmissions = _.filter(this.state.emissions, (o) => {return (new Date(o.datetime) < new Date()) && (o.link != false)});
         latestEmissions = (_.sortBy(latestEmissions, (o) => { return (o['datetime'] || 0)})).reverse().slice(0, 10);
         let homePage = this.state.homePage;
         let infoPage = this.state.infoPage;
